ef6b45ed01601ca8609423a1a04d0c78d48a27c1
Zelak312 <zelak312@gmail.com>
2024-01-03 14:16:14 -0800

    feat: adding rife util

--- rife.go
+++ rife.go
@@ -1,2 +1,14 @@
 package main
 
+import (
+	"context"
+	"os/exec"
+	"path"
+)
+
+func InterpolateVideo(ctx context.Context, binaryPath string, inputPath string, outputPath string, model string) (string, error) {
+	modelPath := path.Join(binaryPath, "../", model)
+	cmd := exec.CommandContext(ctx, binaryPath, "-i", inputPath, "-o", outputPath, "-m", modelPath)
+	output, err := cmd.CombinedOutput()
+	return string(output), err
+}
