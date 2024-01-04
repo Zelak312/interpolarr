00f5e61a58a56dbf018e4938f050c4343b89171b
Zelak312 <zelak312@gmail.com>
2024-01-04 12:10:55 -0800

    fix: remove model path

--- rife.go
+++ rife.go
@@ -3,12 +3,10 @@ package main
 import (
 	"context"
 	"os/exec"
-	"path"
 )
 
 func InterpolateVideo(ctx context.Context, binaryPath string, inputPath string, outputPath string, model string) (string, error) {
-	modelPath := path.Join(binaryPath, "../", model)
-	cmd := exec.CommandContext(ctx, binaryPath, "-i", inputPath, "-o", outputPath, "-m", modelPath)
+	cmd := exec.CommandContext(ctx, binaryPath, "-i", inputPath, "-o", outputPath, "-m", model)
 	output, err := cmd.CombinedOutput()
 	return string(output), err
 }
